93a2363a463962149f7c915d75818b69edf8c67a
kafanasiff <kafanasiff@gmail.com>
2017-08-21 09:06:48 -0700

    Initial commit

---
+++ Assets/Scripts/Artist_Button_Dialog.cs
@@ -0,0 +1,74 @@
+﻿// Project:			Pixel Art Gallery by Anachronic Designs
+// Coder(s):		Kevin Afanasiff
+// Last Updated:	Dec. 9th, 2016
+
+/*
+ * This script controls input for all artist buttons, and opens the appropriate artist dialog when a button is clicked.
+ * */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Artist_Button_Dialog : MonoBehaviour {
+
+	private Ray ray;
+	private RaycastHit hit;
+
+	public List<GameObject> artistDialogs;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Mouse input
+		#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL || UNITY_WEBPLAYER
+		if (Input.GetMouseButtonUp(0)) {
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				GetInput(hit);
+			}
+		}
+		#endif
+
+		// Touch input
+		#if UNITY_ANDROID || UNITY_IOS
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			ray = Camera.main.ScreenPointToRay(Input.GetTouch (0).position);
+			if (Physics.Raycast(ray, out hit)) {
+				GetInput(hit);
+			}
+		}
+		#endif
+	}
+
+	// Controls all artist buttons
+	private void GetInput(RaycastHit hit) {
+		string buttonName = hit.transform.gameObject.name;
+		switch (buttonName) {
+		case "Artist_Button_0":
+			SelectArtist(0);
+			break;
+		case "Artist_Button_1":
+			SelectArtist(1);
+			break;
+		case "Artist_Button_2":
+			SelectArtist (2);
+			break;
+		default:
+			Debug.Log ("Error - Button not recognized");
+			break;
+		}
+	}
+
+	// Display an artist dialog
+	private void SelectArtist(int artistNumber) {
+		//Debug.Log ("Artist" + artistNumber.ToString ());
+		artistDialogs[artistNumber].SetActive (true);
+		gameObject.SetActive(false);
+	}
+
+}
---
+++ Assets/Scripts/Artist_Dialog.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Artist_Dialog : MonoBehaviour {
+
+	private Ray ray;
+	private RaycastHit hit;
+
+	public GameObject artistButtonDialog;
+	public List<GameObject> images;
+	private int currentImage = 0;
+	private Vector3 originalCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+		originalCameraPosition = Camera.main.transform.position;
+		images [currentImage].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		#if UNITY_EDITOR || UNITY_WEBPLAYER || UNITY_STANDALONE
+		if (Input.GetMouseButtonUp(0)) {
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			GetInput (ray);
+		}
+
+		else if (Input.GetMouseButton(0) && Input.touchCount == 0) {
+			PanScreen();
+		}
+		#endif
+
+		// Multi ball button
+		#if UNITY_ANDROID || UNITY_IOS
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			ray = Camera.main.ScreenPointToRay(Input.GetTouch (0).position);
+			GetInput (ray);
+		}
+
+		else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			PanScreen ();
+		}
+		#endif
+	}
+
+	private void GetInput(Ray ray) {
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.gameObject.name == "Button_Back") {
+				Debug.Log ("Back");
+				artistButtonDialog.SetActive (true);
+				gameObject.SetActive (false);
+				ResetCamera();
+			}
+			if (hit.transform.gameObject.name == "Button_Left") {
+				Debug.Log ("Left");
+				PreviousImage ();
+				ResetCamera();
+			}
+			if (hit.transform.gameObject.name == "Button_Right") {
+				Debug.Log ("Right");
+				NextImage ();
+				ResetCamera();
+			}
+		} 
+	}
+
+	private void ResetCamera() {
+		Camera.main.transform.position = originalCameraPosition;
+		Camera.main.orthographicSize = 5;
+	}
+
+	private void PanScreen() {
+
+
+		//Vector3 CameraPos;
+		float speed = 0.1f;
+		float MouseX;
+		float MouseY;
+		MouseX = Input.GetAxis("Mouse X");
+		MouseY = Input.GetAxis("Mouse Y");
+
+		//Debug.Log ("MouseX = " + MouseX.ToString ());
+		#if UNITY_ANDROID || UNITY_IOS
+		if (Input.touchCount > 0) {
+			//Camera.main.backgroundColor = Color.white;
+
+			Vector2 touchDeltaPosition = Input.touches[0].deltaPosition;
+			MouseX = touchDeltaPosition.x;
+			MouseY = touchDeltaPosition.y;
+		}
+		#endif
+
+
+		Camera.main.transform.Translate (-MouseX * speed, -MouseY * speed, 0);
+		/*
+		CameraPos = new Vector3(-MouseX * speed, -MouseY * speed, 0);
+		Camera.main.transform.position += CameraPos;
+		*/
+
+		/*
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.gameObject.name == "TapAnywhere") {
+				Debug.Log ("move camera");
+				#if UNITY_EDITOR || UNITY_WEBPLAYER || UNITY_STANDALONE
+				Camera.main.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, Camera.main.transform.position.z);
+				#endif
+			} 
+		}
+		*/
+	}
+
+	private void PreviousImage() {
+		images [currentImage].SetActive (false);
+		currentImage--;
+		if (currentImage < 0) {
+			currentImage = images.Count - 1;
+		}
+		images [currentImage].SetActive (true);
+	}
+
+	private void NextImage() {
+		images [currentImage].SetActive (false);
+		currentImage++;
+		if (currentImage >= images.Count) {
+			currentImage = 0;
+		}
+		images [currentImage].SetActive (true);
+	}
+
+}
